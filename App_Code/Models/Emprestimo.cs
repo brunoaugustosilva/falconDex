9c99c4c0da15fbdb5b6874456c65f7aae3d81dff
João Almeida <66041633+joaocarlozzz@users.noreply.github.com>
2021-05-02 14:27:30 -0300

    Add files via upload

---
+++ App_Code/Models/Emprestimo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descrição resumida de Equipamento
+/// </summary>
+/// 
+namespace falconDex.Models
+{
+    public class Emprestimo
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Patrimonio { get; set; }
+        public TipoEquipamento Tipo { get; set; }
+        public Status Status { get; set; }
+        public Local Local { get; set; }
+        public Usuario Usuario { get; set; }
+        public Emprestimo Date { get; set; }
+
+        public Emprestimo()
+        {
+            //
+            // TODO: Adicionar lógica do construtor aqui
+            //
+        }
+    }
+}
